c8d7977dd80e8faf7570516ca03c64ea0679d3b4
aleifericsson <alifnafili@gmail.com>
2023-11-16 21:15:12 +0000

    midware

--- l'app.js
+++ l'app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
-app.listen(3000);
 
 //registe view engine
 app.set('view engine', 'ejs');
 app.set('views', 'myviews');
+
+
+app.listen(3000);
+
+app.use((req, res, next) =>{ //app.get & app.use are both middleware, basically one of many cascading functions between the user's request and giving the user a response
+    console.log("middleware running");
+    next(); //allows the rest of the functions to carry on without giving a response.
+})
+
+app.use(express.static("public"));
+app.use(morgan('dev'));
 
 const fs = require('fs');
 let chars;
@@ -14,7 +25,7 @@ fs.readFile("chars.json", (err, data) =>{
     chars = JSON.parse(data);
 })
 
-app.get("/", (req,res) => {
+app.get("/", (req,res) => { 
     res.render("index", {count2: 0}); 
 })
 app.get("/404", (req,res) => {
